16cab2e7ff033551590bebda400990c05c280dbc
Mike Micallef <82095569+MikeMicallef@users.noreply.github.com>
2022-02-14 18:31:28 +0100

    Update HFunctionMisplacedTiles.java

--- HFunctionMisplacedTiles.java
+++ HFunctionMisplacedTiles.java
@@ -11,26 +11,7 @@ import sac.StateFunction;
 		SlidingPuzzle slidingPuzzle = ( SlidingPuzzle ) State;
 		double h = 0.0;
 		int k = 0;
-//		int n=slidingPuzzle .board.length;
-//		byte [][] goalstate = null;
-//		goalstate = new byte [n][n];
-//		
-//		
-//		for (int i = 0; i < n; i++) {
-//			for (int j = 0; j < n; j++) {
-//			goalstate [i][j] = (byte) (k++);
-//			}
-//		}
-//		
-//		for (int i = 0; i < slidingPuzzle .board.length; i++) {
-//			for (int j = 0; j < slidingPuzzle .board.length; j++) {
-//				if (slidingPuzzle .board[i][j] != goalstate[i][j]) {
-//					h= h+1.0;
-//				}
-//			}
-//		}
-//		return h;
-		
+
 		
 		for (int i = 0; i < slidingPuzzle .board.length; i++) {
 			for (int j = 0; j < slidingPuzzle .board.length; j++,k++) {
